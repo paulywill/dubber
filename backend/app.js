71f25351853c57d8bd878a3d9b2f11ab269a5b63
Paul Gamble <pgamble@gmail.com>
2021-05-03 01:00:58 -0400

    backend working

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', './src/pages');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/static', express.static(path.join(`${__dirname}/public`)));
+
+app.get('/', (req, res) => res.send('Home Route'));
+
+const port = process.env.PORT || 8888;
+console.log('DB_URI: ' + process.env.DB_URI);
+mongoose
+  .connect(process.env.DB_URI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Server and Database running on ${port}, http://localhost:${port}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
